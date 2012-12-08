3cdd28b29c35a71721e1157a016f7d66f2d17fde
Johannes <jojo@leopard.liba>
2012-12-08 16:28:07 +0100

    Update German translation

--- zpm_de.ts
+++ zpm_de.ts
@@ -15,44 +15,80 @@
         <translation>Suchen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="126"/>
+        <location filename="mainwindow.ui" line="129"/>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="145"/>
+        <location filename="mainwindow.ui" line="148"/>
         <source>&amp;Apply changes</source>
         <translation>Ä&amp;nderungen übernehmen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="150"/>
+        <location filename="mainwindow.ui" line="153"/>
         <source>Load &amp;XML</source>
         <translation>&amp;XML laden</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="51"/>
+        <location filename="mainwindow.cpp" line="73"/>
         <source>Unable to open file</source>
         <translation>Kann Datei nicht öffnen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="129"/>
+        <location filename="mainwindow.cpp" line="173"/>
+        <location filename="mainwindow.cpp" line="228"/>
+        <location filename="mainwindow.cpp" line="260"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="173"/>
+        <location filename="mainwindow.cpp" line="228"/>
+        <location filename="mainwindow.cpp" line="260"/>
+        <source>Cannot allocate memory for dependency resolution</source>
+        <translation>Kann keinen Speicher für die Abhängigkeitsauflösung reservieren</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="345"/>
+        <source>Install</source>
+        <translation>Installieren</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="348"/>
+        <source>Taboo -- Never install</source>
+        <oldsource>Taboo â Never install</oldsource>
+        <translation>Tabu – Niemals installieren</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="349"/>
+        <source>Do Not install</source>
+        <translation>Nicht installieren</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="355"/>
         <source>Keep</source>
         <translation>Behalten</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="130"/>
+        <location filename="mainwindow.cpp" line="356"/>
         <source>Delete</source>
         <translation>Löschen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="131"/>
+        <location filename="mainwindow.cpp" line="357"/>
         <source>Update</source>
         <translation>Aktualisieren</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="132"/>
-        <source>Force update</source>
+        <location filename="mainwindow.cpp" line="358"/>
+        <source>Update unconditionally</source>
         <translation>Unbedingt aktualisieren</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="359"/>
+        <source>Protected -- never modify</source>
+        <oldsource>Protected â never modify</oldsource>
+        <translation>Geschützt – nicht verändern</translation>
     </message>
 </context>
 <context>
